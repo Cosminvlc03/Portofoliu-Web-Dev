9d859d15cae6d21c38104dad068cff7df9c2f280
Cosminvlc03 <valceacosmin157@gmail.com>
2025-10-27 16:43:04 +0200

    Weather app that use API's to get a city's weather

--- WeatherApp/index.js
+++ WeatherApp/index.js
@@ -17,16 +17,50 @@ app.use('/misc', express.static('misc'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const cityCoordsAPI = "https://geocode.maps.co/";
+const weatherAPI = "https://api.open-meteo.com/v1/";
+
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+let city;
+
 app.post("/submit",(req, res) => {
+  city = req.body.city;
+  console.log(`City submitted: ${city}`);
   res.redirect("/weather");
+
 });
 
-app.get("/weather", (req, res) => {
-  res.render("weather.ejs");
+let weather;
+
+app.get("/weather", async(req, res) => {
+  try{
+    const coordResponse = await axios.get(`${cityCoordsAPI}search`, {
+      params: {
+        q: city,
+        limit: 1
+      }
+    });
+    const { lat, lon } = coordResponse.data[0];
+    const weatherResponse = await axios.get(`${weatherAPI}forecast`, {
+      params: {
+        latitude: lat,
+        longitude: lon,
+        current_weather: true,
+        hourly: 'precipitation_probability'
+        
+      }
+    });
+    weather = weatherResponse.data.current_weather.temperature + "°C" + ", Precipitation Probability: " + weatherResponse.data.hourly.precipitation_probability[0] + "%";
+    console.log(`Weather fetched: ${weather}`);
+    res.render("weather.ejs", { city, weather });
+  }
+  catch (error) {
+    console.error("Error fetching weather data:", error);
+    res.render("weather.ejs", { city });
+  }
 });
 
 app.listen(port, () => {
