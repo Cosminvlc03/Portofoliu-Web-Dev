a18c4d3e6059fbdbb102eb0e3efe8f62ce74ad03
Cosminvlc03 <valceacosmin157@gmail.com>
2025-11-25 16:40:23 +0200

    Made login page FE and some BE logic

--- Movie Tracker App/index.js
+++ Movie Tracker App/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -17,8 +18,44 @@ app.use('/misc', express.static('misc'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "movies",
+  password: "HonestHearts21",
+  port: 5432,
+});
+
+db.connect();
+
 app.get("/", (req, res) => {
   res.render("login.ejs");
+});
+
+app.post("/login", async(req,res) =>{
+  try{
+    const username = req.body.username;
+    const password = req.body.password;
+    const result = await db.query("SELECT * FROM users WHERE username = ($1) AND password = ($2)",[username,password]);
+    let user = result.rows;
+    if(user.length > 0){
+      res.render("home.ejs");
+      console.log("Login succesful")
+    } else {
+      console.log("Invalid username or password");
+      res.render("login.ejs", { error: "Invalid username or password"});
+    }
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+app.post("/forgotten", (req,res) =>{
+  res.render("forgotten.ejs");
+});
+
+app.post("/signup", (req,res) => {
+  res.render("signup.ejs")
 });
 
 app.listen(port, () => {
